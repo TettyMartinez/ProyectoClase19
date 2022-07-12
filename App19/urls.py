5d51c061b6c84ba61fdb764a4d8be4d37b00476e
TettyMartinez <tettymartinez95@gmail.com>
2022-07-12 18:25:07 -0300

    Nuevo de la clase 19

---
+++ App19/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('curso/', curso),
+    path('cursos/', cursos),
+    path('estudiantes/', estudiantes),
+    path('profesores/', profesores),
+    path('entregables/', entregables),
+    path('', inicio),
+]
--- App19/views.py
+++ App19/views.py
@@ -9,4 +9,19 @@ def curso(self):
     doc_texto= f"Curso: {curso.nombre}      Comision: {curso.comision}"
 
     return HttpResponse(doc_texto)
-    
+
+
+def inicio(request):
+    return render(request,'App19/inicio.html')
+
+def cursos(request):
+    return render(request,"App19/cursos.html")
+
+def profesores(request):
+    return render(request,'App19/profesores.html')
+
+def estudiantes(request):
+    return render(request,'App19/estudiantes.html')
+
+def entregables(request):
+    return render(request, 'App19/entregables.html')
